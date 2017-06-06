acab6c82cbd847a352517fee46edf3a36d8e7aa6
soldat76 <aster.fan@gmail.com>
2017-06-06 00:41:05 -0700

    Add files via upload

--- RoomTest1.py
+++ RoomTest1.py
@@ -6,23 +6,28 @@
 ##
 ## June 2017
 
-print("\nYou wake up in a room. There is a DOOR in the room.") # Introductory statement.
+# Introductory statement.
+print("\nYou wake up in a room. There is a DOOR in the room.")
 
-x = 1 # First part starts here.
+# 1st part starts here.
+x = 1
 while x == 1:
     userDecision = input("\nWhat will you do?: ")
     userDecision = userDecision.lower()
+    # Correct user decision should be broad enough.
     if "go to door" in userDecision or "unlock door" in userDecision or "open door" in userDecision:
         print("\n\n\nThe door is locked. There seems to be a KEY on the ground.")
         x = 2
         break
+    # I prefer the user to have an action to go with the word.
     elif "door" in userDecision:
         print("\n\n\nWhat do I do with the door?")
         print("\nThere is an unopened DOOR in the room.")
     else:
         print("\n\n\nI don't understand...")
         print("\nThere is a DOOR in the room.")
-        
+
+# 2nd part starts here.        
 while x == 2:
     userDecision = input("\nWhat will you do?: ")
     userDecision = userDecision.lower()
@@ -30,6 +35,7 @@ while x == 2:
         print("\n\n\nYou pick up the key on the ground.")
         x = 3
         break
+    # Again, I prefer user to enter an action.
     elif "key" in userDecision:
         print("\n\n\nWhat should I do to the key?")
         print("\nThere is a key on the ground.")
